aaf92103fe18ca2197171766ab81e519f3850573
heesu <oung1002@snu.ac.kr>
2021-01-31 01:11:18 +0900

    Add screens, styles,types folder

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>같이 구매 화이팅~~~ 성공하자!!</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/styles/theme.ts
@@ -0,0 +1,12 @@
+// thema 저장 공간
+
+const theme = {
+  palette: {
+    dark: '',
+  },
+  typography: {
+    title: {},
+  },
+};
+
+export default theme;
