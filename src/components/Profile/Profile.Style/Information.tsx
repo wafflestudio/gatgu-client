69f5166d7839a4cd3602fd5d2adbb252c9ca1403
woohm402 <woohm404@gmail.com>
2021-02-05 17:25:33 +0900

    info layout complete

--- src/components/Profile/Profile.Style/Information.tsx
+++ src/components/Profile/Profile.Style/Information.tsx
@@ -3,7 +3,38 @@ import { StyleSheet } from 'react-native';
 const infoStyle = StyleSheet.create({
   container: {
     height: 170,
-    backgroundColor: 'red',
+    borderColor: 'black',
+    borderWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    flexDirection: 'row',
+  },
+  profileImg: {
+    height: 100,
+    width: 100,
+    borderRadius: 60,
+    overflow: 'hidden',
+    marginRight: 25,
+  },
+  detail: {
+    height: 90,
+    width: 100,
+    justifyContent: 'space-between',
+  },
+  content: {
+    height: 90,
+    width: 120,
+    justifyContent: 'space-between',
+  },
+  detailText: {
+    fontSize: 20,
+    color: 'gray',
+    margin: 2,
+  },
+  authView: {},
+  textWrapper: {
+    flexDirection: 'row',
   },
 });
 
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,15 +1,41 @@
-import { View, Text } from 'react-native';
+import { View, Text, ImageBackground } from 'react-native';
 import styles from './Profile.Style';
 import React from 'react';
 
+// TODO:remove
+const dummyInfo = {
+  profileUrl: 'https://reactjs.org/logo-og.png',
+  name: '같구',
+  date: '1920-10-80',
+  auth: false,
+};
+
 const Profile = () => {
+  const profileImg = { uri: dummyInfo.profileUrl };
+
   return (
     <>
       <View style={styles.header.container}>
         <Text>Hi</Text>
       </View>
       <View style={styles.info.container}>
-        <Text>Hi</Text>
+        <ImageBackground source={profileImg} style={styles.info.profileImg} />
+        <View style={styles.info.detail}>
+          <Text style={styles.info.detailText}>유저 이름</Text>
+          <View style={styles.info.authView}>
+            <Text style={styles.info.detailText}>가입일자</Text>
+            <Text style={styles.info.detailText}>인증 여부</Text>
+          </View>
+        </View>
+        <View style={styles.info.content}>
+          <Text style={styles.info.detailText}>{dummyInfo.name}</Text>
+          <View style={styles.info.authView}>
+            <Text style={styles.info.detailText}>{dummyInfo.date}</Text>
+            <Text style={styles.info.detailText}>
+              {dummyInfo.auth ? '인증' : '미인증'}
+            </Text>
+          </View>
+        </View>
       </View>
       <View style={styles.grade.container}>
         <Text>Hi</Text>
