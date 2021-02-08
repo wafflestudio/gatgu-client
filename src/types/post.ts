6dfcca07adddaaf5d9e57a2df2ad79229174248b
heesu <oung1002@snu.ac.kr>
2021-02-08 09:31:44 +0900

    Add types

---
+++ src/types/post.ts
@@ -0,0 +1,11 @@
+export interface IPostProps {
+  id: number;
+  title: string;
+  dayLeft: string;
+  goal: string;
+  location: string;
+  percent: number;
+  uri: string;
+  created: string;
+  money: boolean;
+}
