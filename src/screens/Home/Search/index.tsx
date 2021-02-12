3315af28cf82f25d6b5995d54dddc4a8d4e2b82a
heesu <oung1002@snu.ac.kr>
2021-02-13 00:32:37 +0900

    Add search screen

---
+++ src/screens/Home/Search/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function Search() {
+  return (
+    <View>
+      <Text>서치 서치 서치 마이 네임 구글~</Text>
+    </View>
+  );
+}
+
+export default Search;
