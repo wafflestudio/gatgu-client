930700509b070dcfb8c939ea3c9a5713f6458593
woohm402 <woohm404@gmail.com>
2021-02-12 13:25:07 +0900

    db moved

--- src/apis/PostApi.ts
+++ src/apis/PostApi.ts
@@ -9,10 +9,10 @@ import { IPostProps, IPageLimitRes } from '@/types/post';
 // android에서 local server를 돌리려면 localhost 대신 10.0.2.2를 사용해야 한다. 아이폰 에뮬레이터면 localhost로 바꿔서 테스트 해야한다.
 
 export function _getPosts(page: number): Promise<AxiosResponse<IPostProps[]>> {
-  const url = 'http://10.0.2.2:3000/posts?_limit=7&_page=' + page;
+  const url = 'http://localhost:4000/posts?_limit=7&_page=' + page;
   return requester.get(url);
 }
 
 export function _getPageLimit(): Promise<AxiosResponse<IPageLimitRes>> {
-  return requester.get('http://10.0.2.2:3001/pageLimit');
+  return requester.get('http://localhost:4000/pageLimit');
 }
