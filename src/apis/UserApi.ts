15b0f359c10d946736e07f72b1c8916c88356bc3
woohm402 <woohm404@gmail.com>
2021-02-14 18:24:19 +0900

    UserType => TUserProps

--- src/apis/UserApi.ts
+++ src/apis/UserApi.ts
@@ -1,18 +1,18 @@
 import requester from './BaseInstance';
 import { AxiosResponse } from 'axios';
-import { UserType } from '../types/user';
+import { TUserProps } from '../types/user';
 
-export const readMyInfo = (): Promise<AxiosResponse<UserType>> => {
+export const readMyInfo = (): Promise<AxiosResponse<TUserProps>> => {
   return requester.get('user/me/');
 };
 
-export const logout = (): Promise<AxiosResponse<UserType>> => {
+export const logout = (): Promise<AxiosResponse<TUserProps>> => {
   return requester.put('user/logout/', {});
 };
 
 export const login = (
   username: string,
   password: string
-): Promise<AxiosResponse<UserType>> => {
+): Promise<AxiosResponse<TUserProps>> => {
   return requester.put('user/login/', { username, password });
 };
