24b97fb2300ddc2e477f33b8b1303be0ce0b8786
Hyunsoo Kim <hscornelia@snu.ac.kr>
2021-02-14 21:55:19 +0900

    add fix

--- src/styles/typo.ts
+++ src/styles/typo.ts
@@ -1,6 +1,11 @@
 // 색과 font 정보는 여기서 정의
 
 export const typo = {
+  bigTitle: {
+    fontFamily: 'NotoSansKR_700Bold', // can't find NotoSansCJKkr-Medium
+    lineHeight: 27,
+    fontSize: 18,
+  },
   semiTitle: {
     fontFamily: 'NotoSansKR_400Regular',
     lineHeight: 22,
