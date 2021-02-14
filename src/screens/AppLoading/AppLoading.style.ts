76b656c74dd9905baef7c109f4e1d13e71cecd41
woohm402 <woohm404@gmail.com>
2021-02-14 16:17:38 +0900

    loading screen template complete

---
+++ src/screens/AppLoading/AppLoading.style.ts
@@ -0,0 +1,13 @@
+import { palette } from '@/styles';
+import { StyleSheet } from 'react-native';
+
+const AppLoadingStyles = StyleSheet.create({
+  container: {
+    backgroundColor: palette.yellow,
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default AppLoadingStyles;
--- src/screens/AppLoading/index.tsx
+++ src/screens/AppLoading/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import styles from './AppLoading.style';
 
 function AppLoadingTemplate(): JSX.Element {
   return (
-    <View>
-      <Text>로딩중</Text>
+    <View style={styles.container}>
+      <Text>여기 로고가 들어감</Text>
     </View>
   );
 }
