af0fec782e6a44bb8e45b93775e66ac50ec59369
heesu <oung1002@snu.ac.kr>
2021-02-15 22:46:43 +0900

    Add screens to Routes file

--- src/helpers/routes.tsx
+++ src/helpers/routes.tsx
@@ -1,40 +1,42 @@
-import React from 'react';
-import { TouchableHighlight, Text } from 'react-native';
-import { Home, Notification } from '@/screens';
+import {
+  WriteArticle,
+  ChattingList,
+  Profile,
+  AppLoading,
+  Article,
+  Home,
+  Notification,
+} from '@/screens';
 
-import { createStackNavigator } from '@react-navigation/stack';
-import { useNavigation } from '@react-navigation/native';
+const routes = {
+  Home: {
+    name: 'Home',
+    component: Home,
+  },
+  WriteArticle: {
+    name: 'WriteArticle',
+    component: WriteArticle,
+  },
+  ChattingList: {
+    name: 'ChattingList',
+    component: ChattingList,
+  },
+  Profile: {
+    name: 'Profile',
+    component: Profile,
+  },
+  AppLoading: {
+    name: 'AppLoading',
+    component: AppLoading,
+  },
+  Article: {
+    name: 'Article',
+    component: Article,
+  },
+  Notification: {
+    name: 'Notification',
+    component: Notification,
+  },
+};
 
-const HomeStack = createStackNavigator();
-
-function HomeStackScreen(): JSX.Element {
-  const navigation = useNavigation();
-  return (
-    <HomeStack.Navigator>
-      <HomeStack.Screen
-        name="Home"
-        component={Home}
-        options={{
-          headerTitleAlign: 'center',
-          // eslint-disable-next-line react/display-name
-          headerRight: () => (
-            <TouchableHighlight
-              onPress={() => navigation.navigate('Notification')}
-            >
-              <Text>알림 아이콘</Text>
-            </TouchableHighlight>
-          ),
-        }}
-      />
-      <HomeStack.Screen
-        name="Notification"
-        component={Notification}
-        options={{
-          headerTitleAlign: 'center',
-        }}
-      />
-    </HomeStack.Navigator>
-  );
-}
-
-export { HomeStackScreen };
+export default routes;
