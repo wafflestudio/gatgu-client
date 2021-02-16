a3c0434fea3c212435086f1065e5c61922a738f4
Hyunsoo Kim <hscornelia@snu.ac.kr>
2021-02-17 00:34:16 +0900

    refine addimage part

--- src/screens/WriteArticle/AddImage.style.ts
+++ src/screens/WriteArticle/AddImage.style.ts
@@ -1,9 +1,34 @@
+import { palette } from '@/styles';
 import { StyleSheet } from 'react-native';
 
 const AddImageStyle = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: 100,
+    borderBottomColor: palette.borderGray,
+    borderBottomWidth: 1,
+    justifyContent: 'center',
+  },
+
+  defaultPhoto: {
+    width: 50,
+    height: 50,
+  },
+
   photo: {
     width: '100%',
-    height: 250,
+    height: '100%',
+  },
+
+  photoContainer: {
+    width: '25%',
+    height: '80%',
+    marginLeft: 20,
+    marginTop: 8,
+    borderWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: palette.borderGray,
   },
 });
 
