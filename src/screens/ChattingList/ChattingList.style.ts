380fc44d3e3b48e67f3a5f9bcc48c2cd048b9a94
heesu <oung1002@snu.ac.kr>
2021-02-16 12:23:03 +0900

    Improve chattingList

--- src/screens/ChattingList/ChattingList.style.ts
+++ src/screens/ChattingList/ChattingList.style.ts
@@ -1,11 +1,33 @@
 import { StyleSheet } from 'react-native';
+import { typo, palette } from '@/styles';
+import { flexRow } from '@/styles/wrapper';
 
 const ChattingListStyles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  Head: {
+    ...typo.semiTitle,
+    flexWrap: 'wrap',
+    height: 22,
+  },
+  Box: {
+    width: '70%',
+  },
+  description: {
+    ...typo.info,
+    color: palette.gray,
+    flexWrap: 'wrap',
+    height: 22,
+  },
+  thunmnail: {
+    borderRadius: 32,
+  },
+  textWrapper: {
+    alignItems: 'flex-start',
+    marginLeft: '3.6%',
+  },
+  writerTimeWrapper: {
+    flexDirection: 'row',
+    width: '70%',
+    justifyContent: 'space-between',
   },
 });
 
--- src/screens/ChattingList/index.tsx
+++ src/screens/ChattingList/index.tsx
@@ -1,12 +1,86 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
+import { InfoContainer, Thumbnail, Header } from '@/components';
 
-import styles from './ChattingList.style';
+import ChattingListStyles from './ChattingList.style';
+
+interface INotifyItem {
+  uri: string;
+  title: string;
+  chat: string;
+  time: number;
+  nickName: string;
+}
+
+const mockData: INotifyItem[] = [
+  {
+    uri:
+      'https://search.pstatic.net/sunny/?src=https%3A%2F%2Fi.pinimg.com%2F736x%2Fe6%2F24%2F6b%2Fe6246b2b4032b3d26d8ca49d49001694.jpg&type=ofullfill340_600',
+    title: '물 들어올 때 노 젓자~',
+    time: 1,
+    chat: '이것은 채팅 내용입니다. 이것은 채팅 내용입니다.',
+    nickName: '현수',
+  },
+  {
+    uri:
+      'https://search.pstatic.net/sunny/?src=https%3A%2F%2Fi.pinimg.com%2F736x%2Fe6%2F24%2F6b%2Fe6246b2b4032b3d26d8ca49d49001694.jpg&type=ofullfill340_600',
+    title: '프로틴 같이 사실 분 구해요~',
+    time: 2,
+    chat:
+      '이것은 채팅 내용입니다. 이것은 채팅 내용입니다.이것은 채팅 내용입니다.',
+    nickName: '현민',
+  },
+  {
+    uri:
+      'https://search.pstatic.net/sunny/?src=https%3A%2F%2Fi.pinimg.com%2F736x%2Fe6%2F24%2F6b%2Fe6246b2b4032b3d26d8ca49d49001694.jpg&type=ofullfill340_600',
+    title: '나나나나나나나ㅏ나낭나러ㅏㄴ어ㅏㅇ너ㅏ',
+    time: 10,
+    chat: '이것은 채팅내 용입니다. 이것은 caksdk입니다.이것은 채팅내 용입니다.',
+    nickName: '희수',
+  },
+];
 
 function ChattingListTemplate(): JSX.Element {
+  const renderItem = ({ item }: { item: INotifyItem }) => (
+    <InfoContainer>
+      <Thumbnail
+        uri={item.uri}
+        w={64}
+        h={64}
+        style={ChattingListStyles.thunmnail}
+      />
+      <View style={ChattingListStyles.textWrapper}>
+        <View style={ChattingListStyles.writerTimeWrapper}>
+          <Text style={ChattingListStyles.Head}>{item.nickName}</Text>
+          <Text style={ChattingListStyles.description}>{item.time}시간 전</Text>
+        </View>
+        <View style={ChattingListStyles.Box}>
+          <Text
+            style={ChattingListStyles.Head}
+            ellipsizeMode={'tail'}
+            numberOfLines={1}
+          >
+            {item.chat}
+          </Text>
+          <Text
+            style={ChattingListStyles.description}
+            ellipsizeMode={'tail'}
+            numberOfLines={1}
+          >
+            {item.title}
+          </Text>
+        </View>
+      </View>
+    </InfoContainer>
+  );
+
   return (
-    <View style={styles.container}>
-      <Text>this is chatting</Text>
+    <View>
+      <FlatList
+        data={mockData}
+        renderItem={renderItem}
+        keyExtractor={(_, ind) => String(ind)}
+      />
     </View>
   );
 }
