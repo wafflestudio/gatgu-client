613d6e876f6f2a19d64e41cbc281c68b26c2145b
heesu <oung1002@snu.ac.kr>
2021-02-17 00:50:43 +0900

    Add noticeTemplate

--- src/screens/Notification/index.tsx
+++ src/screens/Notification/index.tsx
@@ -10,12 +10,16 @@ const mockData: INotifyItem[] = [
       'https://search.pstatic.net/sunny/?src=https%3A%2F%2Fi.pinimg.com%2F736x%2Fe6%2F24%2F6b%2Fe6246b2b4032b3d26d8ca49d49001694.jpg&type=ofullfill340_600',
     title: '물 들어올 때 노 젓자~',
     time: 1,
+    noticeType: 'DeadLine',
+    user: '쓰니',
   },
   {
     uri:
       'https://search.pstatic.net/sunny/?src=https%3A%2F%2Fi.pinimg.com%2F736x%2Fe6%2F24%2F6b%2Fe6246b2b4032b3d26d8ca49d49001694.jpg&type=ofullfill340_600',
     title: '프로틴 같이 사실 분 구해요~',
     time: 2,
+    noticeType: 'DeadLine',
+    user: '하이하이',
   },
 ];
 
