65b7848a9cd14e1639eb552e5f04bac21a08fad0
heesu <oung1002@snu.ac.kr>
2021-02-17 20:39:01 +0900

    *

--- src/constants/ErrorCode.ts
+++ src/constants/ErrorCode.ts
@@ -1,3 +1,5 @@
+export const UNKNOWN_ERR = -100;
+
 const defaultErrorMsg = {
   400: '서버에 잘못된 요청을 보내고 있습니다',
   401: '인증되지 않은 사용자입니다',
@@ -5,8 +7,7 @@ const defaultErrorMsg = {
   404: '해당 게시물은 존재하지 않습니다',
   500: '서버에 문제가 있습니다. 잠시 후에 다시 시도해주세요',
   502: '서버에 문제가 있습니다. 잠시 후에 다시 시도해주세요',
+  [UNKNOWN_ERR]: '알 수 없는 문제가 발생했습니다. 잠시 후에 다시 시도해주세요',
 };
-
-export const UNKNOWN_ERR = -100;
 
 export default defaultErrorMsg;
