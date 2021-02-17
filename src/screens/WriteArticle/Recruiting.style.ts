9ea56d3fc38a10e71e7ed20fe09171c2dc71b2eb
Hyunsoo Kim <hscornelia@snu.ac.kr>
2021-02-17 17:54:12 +0900

    modify recruiting part

--- src/screens/WriteArticle/Recruiting.style.ts
+++ src/screens/WriteArticle/Recruiting.style.ts
@@ -1,3 +1,4 @@
+import { palette } from '@/styles';
 import { StyleSheet } from 'react-native';
 
 const TitleStyle = StyleSheet.create({
@@ -6,24 +7,32 @@ const TitleStyle = StyleSheet.create({
     flexWrap: 'wrap',
   },
 
-  recruitHalfContainer: {
-    borderWidth: 1,
-    height: 60,
-    width: '50%',
-    alignItems: 'center',
-    flexDirection: 'row',
-  },
-
-  label: {
-    marginRight: 10,
-    marginLeft: 10,
+  bigContainer: {
+    height: 130,
   },
 
   subContainer: {
     height: 60,
-    borderWidth: 1,
-    flexDirection: 'row',
+    borderBottomWidth: 1,
+    borderBottomColor: palette.borderGray,
+    paddingLeft: 20,
+  },
+
+  switchContainer: {
+    width: '100%',
+    paddingRight: 20,
+    paddingLeft: 20,
+    paddingTop: 20,
     alignItems: 'center',
+    marginTop: 10,
+    height: 60,
+  },
+
+  selectedBox: {
+    borderColor: palette.borderGray,
+    borderWidth: 1,
+    borderRadius: 7,
+    height: 30,
   },
 });
 
--- src/screens/WriteArticle/Recruiting.tsx
+++ src/screens/WriteArticle/Recruiting.tsx
@@ -2,6 +2,11 @@ import { Label } from 'native-base';
 import React, { useState, Dispatch, SetStateAction } from 'react';
 import { View, TextInput } from 'react-native';
 import styles from './Recruiting.style';
+import SwitchSelector, {
+  ISwitchSelectorOption,
+} from 'react-native-switch-selector';
+import { palette, typo } from '@/styles';
+import { StringInput } from '@/components';
 
 interface Props {
   need_people: string;
@@ -9,12 +14,20 @@ interface Props {
   setPeople: Dispatch<SetStateAction<string>>;
   setPrice: Dispatch<SetStateAction<string>>;
 }
+
 function Recruiting({
   need_people,
   need_price,
   setPeople,
   setPrice,
 }: Props): JSX.Element {
+  const [selected, setSelected] = useState(0);
+
+  const options = [
+    { label: '모집인원/필요인원', value: '0' },
+    { label: '모금금액/필요금액', value: '1' },
+  ];
+
   const changeNumber = (txt: string, num: number) => {
     // code for dismissing all letters
     // code for max limit
@@ -22,30 +35,41 @@ function Recruiting({
     else setPrice(txt);
   };
 
+  const Input = (str: string, maxL: number) => {
+    return (
+      <View style={styles.subContainer}>
+        <StringInput
+          style={styles.text}
+          placeholderStyle={styles.text}
+          keyboardType="number-pad"
+          placeholder={str}
+          onChangeText={(txt) => changeNumber(txt, selected)}
+          value={selected == 0 ? need_people : need_price}
+          maxLength={maxL}
+        />
+      </View>
+    );
+  };
+
   return (
-    <View style={styles.subContainer}>
-      <View style={styles.recruitHalfContainer}>
-        <Label style={styles.label}>모집인원: </Label>
-        <TextInput
-          style={styles.text}
-          keyboardType="number-pad"
-          placeholder="모집인원"
-          onChangeText={(txt) => changeNumber(txt, 0)}
-          value={need_people}
-          maxLength={5}
+    <View style={styles.bigContainer}>
+      <View style={styles.switchContainer}>
+        <SwitchSelector
+          options={options}
+          initial={0}
+          onPress={(value) => setSelected(value as number)}
+          height={30}
+          textStyle={{ ...typo.semiTitle }}
+          selectedTextStyle={{ ...typo.semiTitle }}
+          backgroundColor={palette.whiteGray}
+          buttonColor={palette.white}
+          selectedColor={'black'}
+          borderRadius={7}
+          style={{ width: '90%' }}
+          selectedTextContainerStyle={styles.selectedBox}
         />
       </View>
-      <View style={styles.recruitHalfContainer}>
-        <Label style={styles.label}>모금금액: </Label>
-        <TextInput
-          style={styles.text}
-          keyboardType="number-pad"
-          placeholder="모집금액"
-          onChangeText={(txt) => changeNumber(txt, 1)}
-          value={need_price}
-          maxLength={10}
-        />
-      </View>
+      {selected == 0 ? Input('필요인원', 5) : Input('필요금액', 10)}
     </View>
   );
 }
