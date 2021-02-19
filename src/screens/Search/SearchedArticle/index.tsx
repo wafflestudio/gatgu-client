74ff8942ce51d183f22dbbdcb83173b660ba1d57
heesu <oung1002@snu.ac.kr>
2021-02-20 01:53:30 +0900

    Add SearchedArticle screen

---
+++ src/screens/Search/SearchedArticle/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import SearchBar from '../SearchBar';
+import SearchedList from './SearchedList';
+import SearchStyle from '../Search.style';
+
+function SearchListScreen(): JSX.Element {
+  return (
+    <View style={SearchStyle.wrapper}>
+      <View style={SearchStyle.searchWrapper}>
+        <SearchBar inSearchedList={true} />
+      </View>
+
+      <SearchedList />
+    </View>
+  );
+}
+
+export default SearchListScreen;
