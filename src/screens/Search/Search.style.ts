b4b61d65fcd781be415b5591faa6654512653310
heesu <oung1002@snu.ac.kr>
2021-02-20 01:57:48 +0900

    Add search screen
    (cherry picked from commit ea835954d4825f27f3f16c7a9e8d609984945dfb)

--- src/screens/Search/Search.style.ts
+++ src/screens/Search/Search.style.ts
@@ -1,11 +1,52 @@
 import { StyleSheet } from 'react-native';
+import { typo, palette } from '@/styles';
 
 const SearchStyles = StyleSheet.create({
   wrapper: {
-    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
+    width: '100%',
+  },
+  head: {
+    ...typo.bigTitle,
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  tagBox: {
+    borderRadius: 15,
+    minWidth: 54,
+    height: 28,
+    borderColor: palette.borderGray,
+    borderWidth: 1,
     justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 2,
+    marginHorizontal: 5,
+    paddingHorizontal: 13,
+    paddingVertical: 3,
+  },
+  tagText: {
+    ...typo.info,
+    color: palette.whiteGray,
+  },
+  sectionWrapper: {
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+    borderBottomWidth: 1,
+    borderColor: palette.borderGray,
+    width: '100%',
+  },
+  searchWrapper: {
+    width: '100%',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    borderBottomWidth: 1,
+    borderColor: palette.borderGray,
+    paddingHorizontal: 20,
+    paddingVertical: 9,
   },
 });
 
--- src/screens/Search/index.tsx
+++ src/screens/Search/index.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { View } from 'react-native';
 
 import SearchBar from './SearchBar';
-import SearchedList from './SearchedList';
 import SearchStyle from './Search.style';
+import SearchSection from './SearchSection';
 
 function Search(): JSX.Element {
   return (
     <View style={SearchStyle.wrapper}>
-      <SearchBar />
-      <SearchedList />
+      <View style={SearchStyle.searchWrapper}>
+        <SearchBar inSearchedList={false} />
+      </View>
+      <SearchSection />
     </View>
   );
 }
