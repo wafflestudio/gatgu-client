5a283cf981e996ecf06b0126fca42b53c22e7861
woohm402 <woohm404@gmail.com>
2021-02-20 02:56:30 +0900

    waiting for design

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 
 function LoginTemplate(): JSX.Element {
   return (
     <>
-      <View></View>
+      <View>
+        <Text>디자인이 나오지 않아 이 상태인 로그인 창입니다</Text>
+      </View>
     </>
   );
 }
