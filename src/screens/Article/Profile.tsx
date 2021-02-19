df31b59e5b589eadf4c2a5578a44b1fc28f6e6b1
Hyunsoo Kim <hscornelia@snu.ac.kr>
2021-02-20 01:45:31 +0900

    redirect profile pic --> profile page

--- src/screens/Article/Profile.tsx
+++ src/screens/Article/Profile.tsx
@@ -1,15 +1,22 @@
 import { View } from 'native-base';
 import React from 'react';
-import { Text, Image } from 'react-native';
+import { Text, Image, TouchableOpacity } from 'react-native';
 import styles from './Profile.style';
 import { typo } from '@/styles';
 import { IArticleProps } from '@/types/article';
+import { useNavigation } from '@react-navigation/native';
 
 // TODO: change input type
 function Profile({ writer }: IArticleProps): JSX.Element {
+  const navigation = useNavigation();
+  const profileRedirect = () => {
+    navigation.navigate('Profile', { params: writer.profile_id });
+  };
   return (
     <View style={styles.profile}>
-      <Image style={styles.profileImg} source={{ uri: writer.picture }} />
+      <TouchableOpacity onPress={profileRedirect}>
+        <Image style={styles.profileImg} source={{ uri: writer.picture }} />
+      </TouchableOpacity>
       <Text style={{ ...typo.semiTitle }}>{writer.nickname}</Text>
     </View>
   );
