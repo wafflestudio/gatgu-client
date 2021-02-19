cd23054d475032f6bf4bbcf48e8e8916e2a96fd2
woohm402 <woohm404@gmail.com>
2021-02-20 02:40:14 +0900

    rebased

---
+++ src/screens/ProfileModify/index.tsx
@@ -0,0 +1,45 @@
+import { View } from 'native-base';
+import { useState } from 'react';
+import { ImageBackground } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+import styles from './ProfileModify.styles';
+import React from 'react';
+
+// TODO: remove this after API 확정
+const dummyInfo = {
+  profileUrl: 'https://reactjs.org/logo-og.png',
+  name: '같구',
+  date: '1920-10-80',
+  auth: false,
+  grade: 2,
+  emdrmqwltn: 128,
+  dmdekqfbf: 97,
+  e_response_time: 10,
+  worjfogmlakdfbf: 100,
+};
+
+function ProfileModify(): JSX.Element {
+  const [nickname, setNickname] = useState('');
+  const profileImg = { uri: dummyInfo.profileUrl };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.imgContainer}>
+        <View style={styles.profileImgWrap}>
+          <ImageBackground source={profileImg} style={styles.profileImg} />
+          <View style={styles.imgPickBtn}></View>
+        </View>
+      </View>
+      <View style={styles.nickContainer}>
+        <TextInput
+          style={styles.nickInput}
+          value={nickname}
+          onChangeText={(e) => setNickname(e)}
+          placeholder="별명"
+        />
+      </View>
+    </View>
+  );
+}
+
+export default ProfileModify;
