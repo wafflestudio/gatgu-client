be62bb660e168b65a8f59759342d99b9730bddcb
heesu <oung1002@snu.ac.kr>
2021-02-20 23:08:04 +0900

    Add tag onpress function

--- src/screens/Search/SearchSection/PopularSearch/index.tsx
+++ src/screens/Search/SearchSection/PopularSearch/index.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import PopularSearchStyle from './PopularSearch.style';
 import SearchStyle from '../../Search.style';
 import Tag from '@/components/Button';
+import { useKeywordDispatch } from '@/helpers/hooks';
 
 interface IPopularSearchProps {
   tags: string[];
 }
 
 function PopularSearch({ tags }: IPopularSearchProps): JSX.Element {
+  const navigation = useNavigation();
+  const keywordDispatch = useKeywordDispatch();
   const renderedTags = tags.map((tag) => (
     <Tag
       title={tag}
       onPress={() => {
-        console.log('');
+        keywordDispatch(tag);
+        navigation.navigate('SearchedArticle');
       }}
       style={[SearchStyle.tagBox, PopularSearchStyle.tag]}
       textStyle={SearchStyle.tagText}
       key={tag}
     />
   ));
+
   return (
     <View style={SearchStyle.sectionWrapper}>
       <Text style={SearchStyle.head}>인기 검색어</Text>
