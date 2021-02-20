6ecda0e51fb6c36bffcb977d709edca784a64921
heesu <oung1002@snu.ac.kr>
2021-02-20 23:09:02 +0900

    connect redux to Search screen

--- src/screens/Search/index.tsx
+++ src/screens/Search/index.tsx
@@ -6,18 +6,20 @@ import SearchBar from './SearchBar';
 import SearchStyle from './Search.style';
 import SearchSection from './SearchSection';
 import { initSearchData } from '@/store/searchedArticleSlice';
+import { useSelector } from '@/helpers/hooks';
 
 function Search(): JSX.Element {
   const dispatch = useDispatch();
-
+  const keyword = useSelector((state) => state.search.keyword);
   useEffect(() => {
+    // set popular keyword, recent keyword
     dispatch(initSearchData());
   }, []);
 
   return (
     <View style={SearchStyle.wrapper}>
       <View style={SearchStyle.searchWrapper}>
-        <SearchBar inSearchedList={false} keyword={''} />
+        <SearchBar inSearchedList={false} keyword={keyword} />
       </View>
       <SearchSection />
     </View>
