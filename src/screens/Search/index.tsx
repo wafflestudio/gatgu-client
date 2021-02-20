c292586305ba31d872434084bb97b606c8538e97
heesu <oung1002@snu.ac.kr>
2021-02-20 11:19:45 +0900

    unify several sections to one Search section

--- src/screens/Search/index.tsx
+++ src/screens/Search/index.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View } from 'react-native';
+import { useDispatch } from 'react-redux';
 
 import SearchBar from './SearchBar';
 import SearchStyle from './Search.style';
 import SearchSection from './SearchSection';
+import { initSearchData } from '@/store/searchedArticleSlice';
 
 function Search(): JSX.Element {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(initSearchData());
+  }, []);
+
   return (
     <View style={SearchStyle.wrapper}>
       <View style={SearchStyle.searchWrapper}>
-        <SearchBar inSearchedList={false} />
+        <SearchBar inSearchedList={false} keyword={''} />
       </View>
       <SearchSection />
     </View>
