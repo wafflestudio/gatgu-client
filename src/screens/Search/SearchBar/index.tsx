5df06971863390ddcacf842d7b19d1f97a2bdb71
heesu <oung1002@snu.ac.kr>
2021-02-20 11:19:45 +0900

    unify several sections to one Search section

--- src/screens/Search/SearchBar/index.tsx
+++ src/screens/Search/SearchBar/index.tsx
@@ -1,20 +1,27 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { View } from 'react-native';
 import { Icon } from 'native-base';
 import { useNavigation } from '@react-navigation/native';
+
 import SearchBarStyles from './SearchBar.style';
 import { StringInput } from '@/components';
+import { setKeyword } from '@/store/searchedArticleSlice';
 
 interface ISearchBarProps {
   inSearchedList: boolean;
+  keyword: string;
 }
 
-function SearchBar({ inSearchedList }: ISearchBarProps): JSX.Element {
+function SearchBar({ inSearchedList, keyword }: ISearchBarProps): JSX.Element {
+  const dispatch = useDispatch();
   const navigation = useNavigation();
 
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState(keyword);
 
   const onSubmit = () => {
+    dispatch(setKeyword({ keyword: input }));
+    setInput('');
     navigation.navigate('SearchedArticle');
   };
 
@@ -22,13 +29,16 @@ function SearchBar({ inSearchedList }: ISearchBarProps): JSX.Element {
     <View style={SearchBarStyles.outerBox}>
       <Icon name="ios-search" style={SearchBarStyles.searchIcon} />
       <StringInput
-        onTouchStart={() => inSearchedList && navigation.navigate('Search')}
+        onTouchStart={() => {
+          inSearchedList && navigation.navigate('Search');
+        }}
         value={input}
         onChangeText={setInput}
         onSubmitEditing={onSubmit}
         placeholder={'키워드로 검색'}
         style={[SearchBarStyles.text, SearchBarStyles.searchText]}
         placeholderStyle={[SearchBarStyles.text, SearchBarStyles.placeholder]}
+        autoFocus={keyword.length !== 0}
       />
     </View>
   );
