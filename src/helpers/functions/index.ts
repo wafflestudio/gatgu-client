73b2e1cb3387dde825a7123157d6f195cced071c
heesu <oung1002@snu.ac.kr>
2021-02-23 22:34:55 +0900

    Add useKetwordDipatch to asyncStorage feature

--- src/helpers/functions/index.ts
+++ src/helpers/functions/index.ts
@@ -1,3 +1,4 @@
 export { default as customObj } from './object';
 export { default as createError } from './createError';
 export { remainTime, calcTimeDiff } from './time';
+export { default as asyncStorageFunc } from './asyncStorage';
--- src/helpers/hooks/useKeywordDispatch.ts
+++ src/helpers/hooks/useKeywordDispatch.ts
@@ -1,12 +1,15 @@
 import { useDispatch } from 'react-redux';
-import { setKeyword } from '@/store/searchedArticleSlice';
-
+import { setKeyword, addRecentSearch } from '@/store/searchedArticleSlice';
+import { asyncStoragekey } from '@/constants/asyncStorage';
+import { asyncStorageFunc } from '@/helpers/functions';
 type TUseKeywordDispath = () => (keyword: string) => void;
 
 const useKeywordDispatch: TUseKeywordDispath = () => {
   const dispatch = useDispatch();
   return (keyword: string) => {
+    dispatch(addRecentSearch({ keyword }));
     dispatch(setKeyword({ keyword }));
+    asyncStorageFunc.addPropArrElem(asyncStoragekey.RECENT_SEARCH, keyword);
   };
 };
 
