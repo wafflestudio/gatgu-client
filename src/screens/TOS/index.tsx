6824d689e4cc94c685ed5a091a356c3c96816364
woohm402 <woohm404@gmail.com>
2021-02-24 18:10:42 +0900

    TOS feature implemented

--- src/screens/TOS/index.tsx
+++ src/screens/TOS/index.tsx
@@ -1,7 +1,7 @@
 import { View, Text, ScrollView } from 'react-native';
 import { Button } from '@/components';
 import React from 'react';
-import { RouteProp, useRoute } from '@react-navigation/native';
+import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
 import { SignUpStackParamList } from '@/types/navigation';
 import contents from './content';
 import styles from './TOS.style';
@@ -9,12 +9,19 @@ import styles from './TOS.style';
 interface TOSProps {
   title: string;
   isOptional: boolean;
-  confirm: () => void;
+  onPress: () => void;
 }
 
 function TOSTemplate(): JSX.Element {
   const route = useRoute<RouteProp<SignUpStackParamList, 'TOS'>>();
-  const { title, isOptional, confirm }: TOSProps = route.params;
+  const { title, isOptional, onPress }: TOSProps = route.params;
+
+  const navigation = useNavigation();
+
+  const confirm = () => {
+    navigation.goBack();
+    onPress();
+  };
 
   return (
     <View style={styles.container}>
