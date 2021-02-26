0309513a102ad32b3402944ab3cba632209eadd5
woohm402 <woohm404@gmail.com>
2021-02-26 17:09:01 +0900

    assets added

--- src/screens/Profile/UnLogged/index.tsx
+++ src/screens/Profile/UnLogged/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Button } from '@/components';
 import { useNavigation } from '@react-navigation/native';
+import { View, Text } from 'react-native';
+import { Image1, Image2 } from '@/assets/ProfilePage';
 
 // Profile Component
 function ProfileTemplate(): JSX.Element {
   const navigation = useNavigation();
   return (
-    <>
-      {/*TODO: 헤더로 옮겨야 되는 것도 있고 로그인 안됐을때 그것도 있는데 디자인 나오고 나면 Profile Feature에서 구현 */}
+    <View>
+      <Text>로그인으로</Text>
+      <Text>더 다양한 기능을 사용하세요.</Text>
+      <Text>채팅을 통해 거래하거나 나의 등급 등을 알 수 있어요.</Text>
+      <Image1 />
+      <Image2 />
       <Button
         title="로그인 창으로"
         onPress={() => navigation.navigate('Login')}
@@ -16,7 +22,15 @@ function ProfileTemplate(): JSX.Element {
         title="회원가입 창으로"
         onPress={() => navigation.navigate('SignUp')}
       />
-    </>
+      <View>
+        <Button title="약관" onPress={() => alert('not implemented')} />
+        <Text>|</Text>
+        <Button
+          title="개인정보 처리방침"
+          onPress={() => alert('not implemented')}
+        />
+      </View>
+    </View>
   );
 }
 
