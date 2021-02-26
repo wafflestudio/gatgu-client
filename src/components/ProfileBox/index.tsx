030b732384393bf1aca8fb1454a8979e870c25fb
Hyunsoo Kim <hscornelia@snu.ac.kr>
2021-02-27 00:22:18 +0900

    factor out profilebox

--- src/components/ProfileBox/index.tsx
+++ src/components/ProfileBox/index.tsx
@@ -6,20 +6,19 @@ import { typo } from '@/styles';
 import { IArticleProps } from '@/types/article';
 import { useNavigation } from '@react-navigation/native';
 
-// TODO: change input type
-function Profile({ writer }: IArticleProps): JSX.Element {
+function Profile({
+  writer: { profile_id, picture, nickname },
+}: IArticleProps): JSX.Element {
   const navigation = useNavigation();
 
   return (
     <View style={styles.profile}>
       <TouchableOpacity
-        onPress={() =>
-          navigation.navigate('Profile', { params: writer.profile_id })
-        }
+        onPress={() => navigation.navigate('Profile', { params: profile_id })}
       >
-        <Image style={styles.profileImg} source={{ uri: writer.picture }} />
+        <Image style={styles.profileImg} source={{ uri: picture }} />
       </TouchableOpacity>
-      <Text style={{ ...typo.semiTitle }}>{writer.nickname}</Text>
+      <Text style={{ ...typo.semiTitle }}>{nickname}</Text>
     </View>
   );
 }
--- src/screens/Article/ProfileChat.tsx
+++ src/screens/Article/ProfileChat.tsx
@@ -1,7 +1,7 @@
 import { View } from 'native-base';
 import React from 'react';
 import styles from './ProfileChat.style';
-import Profile from './Profile';
+import { Profile } from '@/components';
 import Chat from './Chat';
 import { IArticleProps } from '@/types/article';
 
