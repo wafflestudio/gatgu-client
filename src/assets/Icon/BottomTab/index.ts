15c0a28e99702755a97a5c918fac8f4fc29bf1f7
heesu <oung1002@snu.ac.kr>
2021-02-28 13:49:05 +0900

    Change svg file

--- src/assets/Icon/BottomTab/index.ts
+++ src/assets/Icon/BottomTab/index.ts
@@ -2,7 +2,7 @@ import chat from './chat.svg';
 import etc from './etc.svg';
 import home from './home.svg';
 import search from './search.svg';
-import write from './plusSign.svg';
+import write from './write.svg';
 const BottomTabIcon = { chat, etc, home, search, write };
 
 export default BottomTabIcon;
