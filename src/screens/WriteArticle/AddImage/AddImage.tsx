236a32ade5550c74c5eaadc7cd72da8d19d42f58
Hyunsoo Kim <hscornelia@snu.ac.kr>
2021-03-01 23:37:21 +0900

    style change and logic

--- src/screens/WriteArticle/AddImage/AddImage.tsx
+++ src/screens/WriteArticle/AddImage/AddImage.tsx
@@ -1,68 +1,124 @@
-import React, { Dispatch, SetStateAction } from 'react';
-import { View, Image, TouchableHighlight, Platform } from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
+import React, { Dispatch, SetStateAction, useState } from 'react';
+import { View, Image, TouchableHighlight, Text } from 'react-native';
+// import * as ImagePicker from 'expo-image-picker';
 import styles from './AddImage.style';
 import PlusSign from '@/assets/PlusSign';
+import ImagePicker from 'react-native-image-crop-picker';
+import { ScrollView } from 'react-native-gesture-handler';
+import { palette } from '@/styles';
 
 interface AddImageProps {
-  image: string;
-  setImage: Dispatch<SetStateAction<string>>;
+  images: string[];
+  setImages: Dispatch<SetStateAction<string | null | undefined>[]>;
 }
 
 // TODO:
 // - modify to adding multiple images (using image-crop-picker) after ejection
 
-function AddImage({ image, setImage }: AddImageProps): JSX.Element {
-  const pickImage = async () => {
-    // Get permission to access photo gallery
-    async () => {
-      // confirm that the platform is on ios or android
-      if (Platform.OS !== 'web') {
-        // asynchronously ask for permission, receive status (whether granted permission or not)
-        // status can either be 'granted', 'undetermined' or 'denied
-        const {
-          status,
-        } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-        if (status !== 'granted') {
-          alert('Sorry, we need camera roll permissions to make this work!');
-        }
-      }
-    };
-    // show window to select photo from gallery
-    const result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All, // which type of media that can be picked (in this case, images and videos)
-      allowsEditing: true, // whether we can crop the image, trim the video
-      aspect: [4, 3], // aspect ratio that is kept when editing
-      quality: 1, // maximum quality of compression
-    });
+function AddImage({ images, setImages }: AddImageProps): JSX.Element {
+  const [prev, setPrev] = useState<
+    { mime: string; data: string | null | undefined }[]
+  >([]);
+  // const pickImage = async () => {
+  //   // Get permission to access photo gallery
+  //   async () => {
+  //     // confirm that the platform is on ios or android
+  //     if (Platform.OS !== 'web') {
+  //       // asynchronously ask for permission, receive status (whether granted permission or not)
+  //       // status can either be 'granted', 'undetermined' or 'denied
+  //       const {
+  //         status,
+  //       } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+  //       if (status !== 'granted') {
+  //         alert('Sorry, we need camera roll permissions to make this work!');
+  //       }
+  //     }
+  //   };
+  //   // show window to select photo from gallery
+  //   const result = await ImagePicker.launchImageLibraryAsync({
+  //     mediaTypes: ImagePicker.MediaTypeOptions.All, // which type of media that can be picked (in this case, images and videos)
+  //     allowsEditing: true, // whether we can crop the image, trim the video
+  //     aspect: [4, 3], // aspect ratio that is kept when editing
+  //     quality: 1, // maximum quality of compression
+  //   });
 
-    // if user didn't cancel while picking an image, set state to the picked image's uri
-    if (!result.cancelled) {
-      setImage(result.uri);
-    }
-    // else do nothing
+  //   // if user didn't cancel while picking an image, set state to the picked image's uri
+  //   if (!result.cancelled) {
+  //     setImage(result.uri);
+  //   }
+  //   // else do nothing
+  // };
+
+  const pickImage = () => {
+    const tempArrPrev: { mime: string; data: string | null | undefined }[] = [];
+    const tempArrSend: (string | null | undefined)[] = [];
+    ImagePicker.openPicker({
+      width: 300,
+      height: 400,
+      cropping: true,
+      multiple: true,
+      includeBase64: true,
+      mediaType: 'photo',
+    })
+      .then((images) => {
+        images.forEach((item) => {
+          console.log(item.data?.substring(0, 20));
+          tempArrPrev.push({ mime: item.mime, data: item.data });
+          tempArrSend.push(item.data);
+        });
+      })
+      .then(() => {
+        setPrev(tempArrPrev);
+        setImages(tempArrSend);
+      })
+      .catch(() => {
+        console.log('Error...');
+      });
   };
+
+  const previews =
+    images[0] !== '' &&
+    prev.map(
+      (item, key): JSX.Element => (
+        <View style={styles.photoContainer} key={key}>
+          <Image
+            style={styles.photo}
+            source={{ uri: `data:${item.mime};base64,${item.data}` }}
+          />
+        </View>
+      )
+    );
 
   return (
     <View style={styles.container}>
-      <TouchableHighlight onPress={pickImage}>
-        {image === '' ? (
-          <View style={styles.photoContainer}>
-            <PlusSign style={styles.defaultPhoto} />
-          </View>
-        ) : (
-          <View style={styles.photoContainer}>
-            <Image
-              style={styles.photo}
-              source={{
-                uri: image,
-              }}
-            />
-          </View>
-        )}
-      </TouchableHighlight>
+      <View style={styles.subContainer}>
+        <ScrollView horizontal scrollEnabled={true}>
+          <TouchableHighlight onPress={() => pickImage()}>
+            <View style={styles.plusSignCon}>
+              <PlusSign style={styles.defaultPhoto} />
+            </View>
+          </TouchableHighlight>
+          {images[0] !== '' && previews}
+        </ScrollView>
+      </View>
     </View>
   );
 }
 
 export default AddImage;
+
+/*<ScrollView horizontal contentContainerStyle={styles.container} scrollEnabled={true}>
+        <TouchableHighlight onPress={() => pickImage()}>
+          <View style={styles.plusSignCon}>
+            <PlusSign style={styles.defaultPhoto} />
+          </View>
+        </TouchableHighlight>
+        {/* {console.log(previews)} }
+        {images[0] !== '' && previews}
+        {/* <Text style={{marginRight: 10, paddingRight: 30, backgroundColor: 'blue', height: 100}}>Hello1</Text>
+        <Text style={{marginRight: 10, paddingRight: 30, backgroundColor: 'blue'}}>Hello2</Text>
+        <Text style={{marginRight: 10, paddingRight: 30, backgroundColor: 'blue'}}>Hello3</Text>
+        <Text style={{marginRight: 10, paddingRight: 30, backgroundColor: 'blue'}}>Hello4</Text>
+        <Text style={{marginRight: 10, paddingRight: 30, backgroundColor: 'blue'}}>Hello5</Text>
+        <Text style={{marginRight: 10, paddingRight: 30, backgroundColor: 'blue'}}>Hello6</Text> }
+      </ScrollView>*/
