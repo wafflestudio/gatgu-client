07f9d2348700d7d5211a6245525ff1efadd2ad9e
heesu <oung1002@snu.ac.kr>
2021-03-06 02:37:08 +0900

    Apply bubble design

--- src/screens/ChattingRoom/ChatsContainer/ChatBox/Bubble/Bubble.style.tsx
+++ src/screens/ChattingRoom/ChatsContainer/ChatBox/Bubble/Bubble.style.tsx
@@ -1,8 +1,29 @@
 import { StyleSheet } from 'react-native';
+import { palette } from '@/styles';
+
+const commonStyle = StyleSheet.create({
+  chatBox: {
+    borderRadius: 11,
+    paddingVertical: 5,
+    paddingHorizontal: 9,
+    marginTop: 5,
+  },
+});
 
 const BubbleStyle = StyleSheet.create({
-  leftBubble: {},
-  rightBubble: {},
+  leftBubble: {
+    ...commonStyle.chatBox,
+    backgroundColor: palette.white,
+  },
+  rightBubble: {
+    ...commonStyle.chatBox,
+    backgroundColor: palette.yellow,
+  },
+  chatText: {
+    color: palette.dark,
+    fontSize: 15,
+    lineHeight: 22,
+  },
 });
 
 export default BubbleStyle;
