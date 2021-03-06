91503f603c9393fcc4435e29c78d53107b0a7d3d
heesu <oung1002@snu.ac.kr>
2021-03-06 18:25:07 +0900

    Apply getArticleSum actions
    home 컴포넌트와 getArticleSum action 연결

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,17 @@
-import React, { useEffect } from 'react';
-import { FlatList, View } from 'react-native';
+import React, { useCallback, useEffect } from 'react';
+import {
+  FlatList,
+  View,
+  NativeSyntheticEvent,
+  NativeScrollEvent,
+} from 'react-native';
 import { useSelector, useDispatch, shallowEqual } from 'react-redux';
 
 import { ArticleBox } from '@/components';
 import styles from './Home.style';
 import { IArticleSumProps } from '@/types/article';
 import { RootState } from '@/store';
-import { getArticlesPerPage } from '@/store/articleSlice';
+import { getArticlesSum } from '@/store/articleSlice';
 import { createError } from '@/helpers/functions';
 
 const [Error] = createError();
@@ -14,39 +19,45 @@ const [Error] = createError();
 function HomeTemplate(): JSX.Element {
   const dispatch = useDispatch();
 
-  const { data: posts, hasError } = useSelector(
+  const { data: articles, hasError, errorStatus } = useSelector(
     (state: RootState) => state.article,
     shallowEqual
   );
 
   // 초기에 7개 렌더링
   useEffect(() => {
-    dispatch(getArticlesPerPage());
+    dispatch(getArticlesSum('first'));
   }, []);
 
   const renderArticle = ({ item }: { item: IArticleSumProps }) => (
     <ArticleBox {...item} />
   );
 
-  // posts가 바뀌는 경우만 재 렌더링, 0.9 threshold에 다다르면 그 다음 페이지에 해당하는
-  // 포스트를 받아온다.
   const ArticleList = (
     <FlatList
-      data={posts}
+      data={articles}
       renderItem={renderArticle}
       keyExtractor={(_, ind) => String(ind)}
+      onRefresh={() => {
+        getArticlesSum('first');
+      }}
       onEndReached={() => {
-        getArticlesPerPage();
+        getArticlesSum('next');
       }}
       onEndReachedThreshold={0.5}
+      onScroll={(event: NativeSyntheticEvent<NativeScrollEvent>) =>
+        onContentOffsetChanged(event.nativeEvent.contentOffset.y)
+      }
     />
   );
 
+  const onContentOffsetChanged = useCallback((distanceFromTop: number) => {
+    distanceFromTop === 0 && getArticlesSum('previous');
+  }, []);
+
   return (
     <View style={styles.root}>
-      {/* Error 안에 error status code 넣기 */}
-      {/* 임시로 401 넣어 놓음 */}
-      {hasError ? Error(401) : ArticleList}
+      {hasError ? Error(errorStatus) : ArticleList}
     </View>
   );
 }
