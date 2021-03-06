46043a6e4b5314ca733cf052ca58e91b5ee6e652
heesu <oung1002@snu.ac.kr>
2021-03-06 16:20:39 +0900

    fix convention/by review

--- src/screens/ChattingRoom/index.tsx
+++ src/screens/ChattingRoom/index.tsx
@@ -3,7 +3,7 @@ import { View } from 'react-native';
 import ChatsContainer from './ChatsContainer';
 import messages from './mockChat';
 
-export default function ChattingRoom() {
+export default function ChattingRoom(): JSX.Element {
   return (
     <View>
       <ChatsContainer chatList={messages} />
