19b9aa134cdc653b225ab3b7cda4cc1fc21f8361
heesu <oung1002@snu.ac.kr>
2021-03-06 18:15:49 +0900

    Refactor getArticleSum api
    페이지네이션 가능하도록 getArticleSum api 수정

--- src/apis/ArticleApi.ts
+++ src/apis/ArticleApi.ts
@@ -1,26 +1,22 @@
 // thunk functions that return promises
 import { AxiosResponse } from 'axios';
+import qs from 'queryString';
 import requester from './BaseInstance';
 import {
-  IArticleSumProps,
   IArticleProps,
   IMessageRet,
   IArticleSumResponse,
 } from '@/types/article';
+import { PAGE_SIZE } from '@/constants/Enum';
 
-// for home page
-export const readAll = (
-  page: number
-): Promise<AxiosResponse<IArticleSumProps[]>> => {
-  // TODO: @ssu1018
-  const url = `posts?_limit=7&_page=${page}`;
-  return requester.get(url);
-};
-
-export const getArticleSummary = (
-  url: string
+export const getArticlesSummary = (
+  url?: string | null
 ): Promise<AxiosResponse<IArticleSumResponse>> => {
-  return requester.get(url);
+  const query = qs.stringify({
+    page_size: PAGE_SIZE,
+  });
+  url = url || 'article/';
+  return requester.get(`${url}?${query}`);
 };
 
 // for article page
--- src/constants/Enum.ts
+++ src/constants/Enum.ts
@@ -15,3 +15,7 @@ export enum Status {
   WAITING_SHARE = 6,
   GATGU_COMPLETE = 7,
 }
+
+// used in articleSum
+export const MAX_ARTICLE_NUM = 200;
+export const PAGE_SIZE = 10;
