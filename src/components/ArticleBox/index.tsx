37e407922d23b7bf1516889e99833c668ee6d87b
heesu <oung1002@snu.ac.kr>
2021-03-06 18:19:25 +0900

    Refactor goalBar based on api doc

--- src/components/ArticleBox/index.tsx
+++ src/components/ArticleBox/index.tsx
@@ -7,8 +7,12 @@ import styles from './ArticleBox.style';
 import Thumbnail from '@/components/Thumbnail';
 import GoalBar from './GoalBar';
 import { IArticleSumProps } from '@/types/article';
+<<<<<<< HEAD
 import { remainTime, calcTimeDiff } from '@/helpers/functions/time';
 import { IS_PEOPLE } from '@/constants/Enum';
+=======
+import { remainTime } from '@/helpers/functions/time';
+>>>>>>> b8ecb74... Refactor goalBar based on api doc
 
 function ArticleBox({
   article_id,
@@ -16,11 +20,19 @@ function ArticleBox({
   time_in,
   price_min,
   people_min,
+<<<<<<< HEAD
   written_at,
   location,
   thumbnail_url,
   need_type,
   participants_summary,
+=======
+  created_at,
+  location,
+  thumbnail_url,
+  need_type,
+  participnats_summary,
+>>>>>>> b8ecb74... Refactor goalBar based on api doc
 }: IArticleSumProps): JSX.Element {
   const navigation = useNavigation();
 
@@ -43,16 +55,25 @@ function ArticleBox({
       <View style={styles.articleBox}>
         <Text style={styles.Head}>{title}</Text>
         <View style={styles.infoWrapper}>
+<<<<<<< HEAD
           <Text style={styles.description}>
             {`${timeBefore} ${typeBefore} 전`} ·{' '}
           </Text>
+=======
+          <Text style={styles.description}>{created_at} · </Text>
+>>>>>>> b8ecb74... Refactor goalBar based on api doc
           <Text style={styles.description}>{remainTime(time_in)} · </Text>
           <Text style={styles.description}>{location}</Text>
         </View>
         <View style={styles.goalWrapper}>
           <GoalBar
+<<<<<<< HEAD
             summary={participants_summary}
             min_required={need_type === IS_PEOPLE ? people_min : price_min}
+=======
+            summary={participnats_summary}
+            goal={{ price_min, people_min }}
+>>>>>>> b8ecb74... Refactor goalBar based on api doc
             type={need_type}
           />
         </View>
