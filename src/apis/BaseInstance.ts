091bc2505b76f5d05e635360565b6b167bf63e30
Hyunsoo Kim <hscornelia@snu.ac.kr>
2021-03-07 12:30:40 +0900

    temp local server 서버가 안 돼서 일단 json-server 사용함

--- src/apis/BaseInstance.ts
+++ src/apis/BaseInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const requester = axios.create({
-  baseURL: 'http://localhost:8000/v1/',
+  baseURL: 'http://localhost:4000/',
 });
 
 requester.defaults.xsrfCookieName = 'csrftoken';
