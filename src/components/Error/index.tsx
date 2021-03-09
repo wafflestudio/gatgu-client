b35d282bd39e6de31019bdade25e6920878c46ac
heesu <oung1002@snu.ac.kr>
2021-03-10 00:56:21 +0900

    Add TODO info

--- src/components/Error/index.tsx
+++ src/components/Error/index.tsx
@@ -7,6 +7,10 @@ interface IErrorProps {
   status: number;
 }
 
+// TODO: @ssu1018
+// - change status to statusMsg
+// when: ~3월 말
+
 function Error({ status }: IErrorProps): JSX.Element {
   return (
     <View style={ErrorStyles.container}>
