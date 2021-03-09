ffeab89a8d81511a45149374fd78754fd8719b65
heesu <oung1002@snu.ac.kr>
2021-03-10 00:37:52 +0900

    Apply header style
    Add safeArea style, apply header style, change stype props type

--- src/components/Header/Header.style.ts
+++ src/components/Header/Header.style.ts
@@ -1,29 +1,43 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Platform, StatusBar } from 'react-native';
+
+import { palette, typo } from '@/styles';
+
+const commonStyle = StyleSheet.create({
+  button: {
+    width: 38,
+    height: 38,
+  },
+});
 
 const HeaderStyles = StyleSheet.create({
   header: {
-    height: 75,
-    borderWidth: 1,
+    height: 59,
+    borderBottomWidth: 1,
+    borderBottomColor: palette.borderGray,
     flexDirection: 'row',
     alignItems: 'center',
+    backgroundColor: palette.white,
+    justifyContent: 'space-between',
   },
-  subContainer: {
-    width: '33%',
+  basicTitleText: {
+    ...typo.semiTitle,
+  },
+  titleContainer: {
     alignItems: 'center',
     justifyContent: 'center',
   },
-  button: {
-    height: 50,
-    width: 50,
-    borderColor: 'black',
-    borderWidth: 1,
-    borderRadius: 25,
-    alignItems: 'center',
+  SafeArea: {
+    marginTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+  },
+  leftButton: {
+    ...commonStyle.button,
+    marginLeft: 13,
     justifyContent: 'center',
   },
-  title: {
+  rightButton: {
+    ...commonStyle.button,
+    marginRight: 13,
     justifyContent: 'center',
-    alignItems: 'center',
   },
 });
 
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,22 +3,25 @@ import {
   View,
   Text,
   TouchableHighlight,
-  Alert,
-  StyleSheetProperties,
+  StyleProp,
+  ViewStyle,
+  TextStyle,
 } from 'react-native';
+
 import styles from './Header.style';
 
 // need to pass functions for the buttons
 export interface IHeaderProps {
   title: string;
   titleShown?: boolean;
-  titleStyle?: StyleSheetProperties;
+  titleStyle?: StyleProp<TextStyle>;
+  titleContainerStyle?: StyleProp<ViewStyle>;
   left?: React.ReactNode;
   leftCallback?: any;
-  leftStyle?: StyleSheetProperties;
+  leftStyle?: StyleProp<ViewStyle>;
   right?: React.ReactNode;
   rightCallback?: any;
-  rightStyle?: StyleSheetProperties;
+  rightStyle?: StyleProp<ViewStyle>;
 }
 
 function Header({
@@ -31,32 +34,35 @@ function Header({
   rightCallback,
   leftStyle,
   rightStyle,
+  titleContainerStyle,
 }: IHeaderProps): JSX.Element {
   return (
-    <View style={styles.header}>
-      <View style={styles.subContainer}>
-        {left && (
-          <TouchableHighlight
-            style={styles.button}
-            onPress={() => Alert.alert('Goback!')}
-          >
-            <Text>뒤로</Text>
-          </TouchableHighlight>
-        )}
-      </View>
-      <View style={styles.subContainer}>
-        <Text style={styles.title}>{title}</Text>
-      </View>
-      <View style={styles.subContainer}>
-        {right && (
-          <TouchableHighlight
-            style={styles.button}
-            onPress={() => Alert.alert('Complete!')}
-          >
-            <Text>설정</Text>
-          </TouchableHighlight>
-        )}
-      </View>
+    <View style={[styles.header, styles.SafeArea]}>
+      {left ? (
+        <TouchableHighlight
+          style={[styles.leftButton, leftStyle]}
+          onPress={leftCallback}
+        >
+          {left}
+        </TouchableHighlight>
+      ) : (
+        <View style={styles.leftButton} />
+      )}
+      {titleShown && (
+        <View style={[styles.titleContainer, titleContainerStyle]}>
+          <Text style={[styles.basicTitleText, titleStyle]}>{title}</Text>
+        </View>
+      )}
+      {right ? (
+        <TouchableHighlight
+          style={[styles.rightButton, rightStyle]}
+          onPress={rightCallback}
+        >
+          {right}
+        </TouchableHighlight>
+      ) : (
+        <View style={styles.rightButton} />
+      )}
     </View>
   );
 }
