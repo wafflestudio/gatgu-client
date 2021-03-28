d1a382d0ac9ce8f87119dd76c11967d4c751e1e9
Hyunsoo Kim <hscornelia@snu.ac.kr>
2021-03-29 00:10:01 +0900

    add due date modal

--- src/screens/WriteArticle/DueDate/DueDate.style.ts
+++ src/screens/WriteArticle/DueDate/DueDate.style.ts
@@ -1,20 +1,62 @@
 import { StyleSheet } from 'react-native';
 
-import { palette } from '@/styles';
+import { palette, typo } from '@/styles';
 
 import waStyles from '../WriteArticle.style';
 
 const DescriptionStyle = StyleSheet.create({
-  placeHolder: {
-    ...waStyles.placeHolder,
-    marginTop: 20,
+  labelContainer: {
+    height: 50,
+  },
+  label: {
+    ...typo.bigTitle,
+    marginLeft: 20,
   },
 
-  bigContainer: {
-    borderBottomWidth: 1,
-    borderBottomColor: palette.borderGray,
-    maxHeight: 500,
-    minHeight: 175,
+  modalView: {
+    height: '50%',
+    width: '100%',
+    marginTop: 'auto',
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+
+  scrollDayContainer: {
+    width: '100%',
+    alignItems: 'center',
+  },
+
+  dayContainer: {
+    borderWidth: 1,
+    borderRadius: 10,
+    margin: 5,
+    width: 50,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  dayTextSelected: {
+    color: palette.blue,
+  },
+  dayTextNonSelected: {
+    color: palette.gray,
+  },
+  selected: {
+    borderColor: palette.borderBlue,
+  },
+  nonSelected: {
+    borderColor: palette.gray,
   },
 });
 
--- src/screens/WriteArticle/DueDate/DueDate.tsx
+++ src/screens/WriteArticle/DueDate/DueDate.tsx
@@ -1,7 +1,23 @@
-import React, { Dispatch, SetStateAction } from 'react';
-import { View } from 'react-native';
+import React, { Dispatch, SetStateAction, useState } from 'react';
+import {
+  View,
+  TouchableHighlight,
+  Text,
+  Modal,
+  Alert,
+  Platform,
+} from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+
+import _ from 'lodash';
+import { Label } from 'native-base';
+
+// import DateTimePickerModal from "react-native-modal-datetime-picker";
+// npm i react-native-modal-datetime-picker @react-native-community/datetimepicker
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 import { StringInput } from '@/components';
+import { palette } from '@/styles';
 
 import waStyles from '../WriteArticle.style';
 import styles from './DueDate.style';
@@ -10,8 +26,117 @@ interface DueDateProps {
   dueDate: string;
   setDueDate: Dispatch<SetStateAction<string>>;
 }
+const dayOfWeek = ['일', '월', '화', '수', '목', '금', '토'];
+const returnArrayDate = (today: Date) => {
+  const arr = [];
+  const current: Date = today;
+  arr.push({ day: '오늘', date: current.getDate(), selected: false });
+  for (let index = 0; index < 5; index++) {
+    current.setDate(current.getDate() + 1);
+    arr.push({
+      day: dayOfWeek[current.getDay()],
+      date: current.getDate(),
+      selected: false,
+    });
+  }
+  return arr;
+};
+const today = new Date();
+const initWeek = returnArrayDate(today);
 
 function DueDate({ dueDate, setDueDate }: DueDateProps): JSX.Element {
-  return <View></View>;
+  const [modalVisible, setModalVisible] = useState(false);
+  const [date, setDate] = useState(new Date(1598051730000));
+  const [dayArr, setDayArr] = useState(initWeek);
+
+  const onChange = (selectedDate: any) => {
+    const currentDate = selectedDate || date;
+    setDate(currentDate);
+  };
+  const showDatePicker = () => {
+    setModalVisible(!modalVisible);
+  };
+  const selectDay = (indx: number) => {
+    const temp = _.cloneDeep(initWeek);
+    temp[indx].selected = true;
+    setDayArr(temp);
+  };
+
+  const renderDates = dayArr.map((item, indx) => (
+    <TouchableHighlight key={indx} onPress={() => selectDay(indx)}>
+      <View
+        style={[
+          styles.dayContainer,
+          item.selected ? styles.selected : styles.nonSelected,
+        ]}
+      >
+        <Text
+          style={
+            item.selected ? styles.dayTextSelected : styles.dayTextNonSelected
+          }
+        >
+          {item.day}
+        </Text>
+        <Text
+          style={
+            item.selected ? styles.dayTextSelected : styles.dayTextNonSelected
+          }
+        >
+          {item.date}
+        </Text>
+      </View>
+    </TouchableHighlight>
+  ));
+
+  return (
+    <View>
+      <View style={styles.labelContainer}>
+        <TouchableHighlight
+          onPress={() => showDatePicker()}
+          underlayColor={palette.whiteGray}
+        >
+          <Label style={styles.label}>모집기한</Label>
+        </TouchableHighlight>
+      </View>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.modalView}>
+          <TouchableHighlight onPress={() => setModalVisible(false)}>
+            <Text>Close</Text>
+          </TouchableHighlight>
+          <View style={styles.scrollDayContainer}>
+            <ScrollView horizontal={true}>{renderDates}</ScrollView>
+          </View>
+          <View style={{ width: '100%', height: '100%' }}>
+            <DateTimePicker
+              testID="dateTimePicker"
+              value={date}
+              mode={'time'}
+              is24Hour={true}
+              display="spinner"
+              minuteInterval={30}
+              onChange={onChange}
+            />
+          </View>
+        </View>
+      </Modal>
+    </View>
+  );
 }
 export default DueDate;
+
+{
+  /* <DateTimePickerModal
+      isVisible={isDatePickerVisible}
+      mode="time"
+      onConfirm={handleConfirm}
+      onCancel={hideDatePicker}
+    /> */
+}
