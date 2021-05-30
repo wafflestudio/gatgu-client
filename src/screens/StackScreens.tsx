1efb802b31ff226ef7fb400ea7cdaa8e1e6e1574
woohm402 <woohm404@gmail.com>
2021-05-31 01:54:02 +0900

    state.logged => state.token

--- src/screens/StackScreens.tsx
+++ src/screens/StackScreens.tsx
@@ -108,7 +108,7 @@ function HomeStackScreen(): JSX.Element {
 const ProfileStack = createStackNavigator();
 function ProfileStackScreen(): JSX.Element {
   const [show, setShow] = useState(false);
-  const logged = useSelector((state: RootState) => state.user.logged);
+  const logged = !!useSelector((state: RootState) => state.user.token);
   const navigation = useNavigation();
   const dispatch = useDispatch();
 
