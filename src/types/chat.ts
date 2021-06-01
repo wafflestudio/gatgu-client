62aeaab541c0f243ca0dccaa00234203e1f5d0db
Hyunsoo Kim <hscornelia@snu.ac.kr>
2021-06-02 05:22:33 +0900

    added order_chat member to IArticleProps + appropriate changes
    made a new IOrderChat interface - can be merged with IChattingRoom later

--- src/types/chat.ts
+++ src/types/chat.ts
@@ -1,3 +1,5 @@
+import { OrderStatus } from '@/constants/Enum';
+
 import { IChatUserProps, IUserProfile } from './user';
 
 export interface IChattingRoom {
@@ -5,13 +7,23 @@ export interface IChattingRoom {
   participant_profile: IChatUserProps[];
   article: number;
   order_status: number;
-  trackingNumber: string;
+  tracking_number: number;
   // not in api
   uri: string;
   title: string;
   chat: string;
   time: number;
   nickName: string;
+}
+
+// TODO: @ssu1018 @juimdpp
+// Chatting API 확정되면 IChattingRoom이랑 합칠지 말지 결정하기
+export interface IOrderChat {
+  id: number;
+  participant_profile: IChatUserProps[];
+  article: number;
+  order_status: number;
+  tracking_number: number;
 }
 
 export interface IChangeStatusProps {
