d6c264bed9fb5b47bea1569fcce9e961db25265a
heesu <oung1002@snu.ac.kr>
2021-06-20 23:14:58 +0900

    initialize user Gatgu list page

--- src/screens/UserGatgu/UserGatgu.tsx
+++ src/screens/UserGatgu/UserGatgu.tsx
@@ -1,24 +1,79 @@
-import React from 'react';
-import { useInfiniteQuery } from 'react-query';
+import React, { useEffect } from 'react';
 
-import { Container, Header, ScrollableTab, Tab, Tabs } from 'native-base';
+import { Container, ScrollableTab, Tab, Tabs } from 'native-base';
 
-import UserGatguGathering from './UserGatguGathering';
-import UserGatguParticipating from './UserGatguParticipating';
+import { articleAPI } from '@/apis';
+import { ArticleBox, CursorFlatList } from '@/components';
+import { useCursorPagination } from '@/helpers/hooks';
+import { IArticleSummary } from '@/types/article';
 
 const UserGatgu: React.FC = () => {
-  //   const a = useInfiniteQuery();
+  const {
+    isFirstPage: isParticipatedFirstPage,
+    items: participatedItems,
+    refreshing: refreshingParticipatedItems,
+    fetching: fetchingParticipatedItems,
+    getItems: getParticipatedItems,
+  } = useCursorPagination<IArticleSummary>({
+    fetchFunc: articleAPI.getArticles,
+  });
+
+  const {
+    isFirstPage: isHostedFirstPage,
+    items: hostedItems,
+    refreshing: refreshingHostedItems,
+    fetching: fetchingHostedItems,
+    getItems: getHostedItems,
+  } = useCursorPagination<IArticleSummary>({
+    fetchFunc: articleAPI.getArticles,
+  });
+
+  // 초기 렌더링
+  useEffect(() => {
+    getHostedItems('first');
+    getParticipatedItems('first');
+  }, []);
+
+  const renderArticle = ({ item }: { item: IArticleSummary }) => (
+    <ArticleBox {...item} />
+  );
+
+  const renderUserGatguList = (type: 'hosted' | 'participated') => {
+    const isHostedItemsTab = type === 'hosted';
+
+    const items = isHostedItemsTab ? hostedItems : participatedItems;
+    const refreshing = isHostedItemsTab
+      ? refreshingHostedItems
+      : refreshingParticipatedItems;
+    const fetching = isHostedItemsTab
+      ? fetchingHostedItems
+      : fetchingParticipatedItems;
+
+    const isFirstPage = isHostedItemsTab
+      ? isHostedFirstPage
+      : isParticipatedFirstPage;
+    const getItems = isHostedItemsTab ? getHostedItems : getParticipatedItems;
+
+    return (
+      <CursorFlatList
+        items={items}
+        refreshing={refreshing}
+        isFirstPage={isFirstPage}
+        fetching={fetching}
+        getItems={getItems}
+        renderItem={renderArticle}
+      />
+    );
+  };
 
   return (
     <Container>
-      <Header hasTabs />
-      <Tabs renderTabBar={() => <ScrollableTab />} />
-      <Tab heading="내가 참여한 같구">
-        <UserGatguParticipating />
-      </Tab>
-      <Tab heading="내가 모집한 같구">
-        <UserGatguGathering />
-      </Tab>
+      <Tabs renderTabBar={() => <ScrollableTab />}>
+        <Tab heading="내가 참여한 같구">
+          {renderUserGatguList('participated')}
+        </Tab>
+        <Tab heading="내가 모집한 같구">{renderUserGatguList('hosted')}</Tab>
+      </Tabs>
     </Container>
   );
 };
