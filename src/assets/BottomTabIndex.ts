ab2c160c3f71f199aa44bc8e89de424c7fbbd369
Hyunsoo Kim <hscornelia@snu.ac.kr>
2021-06-27 16:32:41 +0900

    4차 - icons

---
+++ src/assets/BottomTabIndex.ts
@@ -0,0 +1,25 @@
+import chat from './icons/Chat/chat.svg';
+import chatDark from './icons/ChatDark/chatDark.svg';
+import etc from './icons/Etc/etc.svg';
+import etcDark from './icons/EtcDark/etcDark.svg';
+import home from './icons/Home/home.svg';
+import homeDark from './icons/HomeDark/homeDark.svg';
+import notification from './icons/Notification/notification.svg';
+import search from './icons/Search/search.svg';
+import searchDark from './icons/SearchDark/searchDark.svg';
+import write from './icons/Write/write.svg';
+import writeDark from './icons/WriteDark/writeDark.svg';
+
+export const HomeIcon = { notification };
+export const BottomTabIcon = {
+  home,
+  chat,
+  search,
+  write,
+  etc,
+  homeDark,
+  chatDark,
+  etcDark,
+  writeDark,
+  searchDark,
+};
