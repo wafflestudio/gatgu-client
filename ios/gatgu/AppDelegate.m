48cacfcd20985748f3c33b4fac1b3cafa163cc32
Hyunsoo Kim <hscornelia@snu.ac.kr>
2021-06-28 14:19:52 +0900

    added firebase/auth

--- ios/gatgu/AppDelegate.m
+++ ios/gatgu/AppDelegate.m
@@ -1,4 +1,5 @@
 #import "AppDelegate.h"
+#import <Firebase.h>
 
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
@@ -41,6 +42,9 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  if ([FIRApp defaultApp] == nil) {
+        [FIRApp configure];
+      }
 #if defined(FB_SONARKIT_ENABLED) && __has_include(<FlipperKit/FlipperClient.h>)
   InitializeFlipper(application);
 #endif
