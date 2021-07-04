99ed2e9cb82a2e4acb0e0d0897f39d5360c5ad71
heesu <oung1002@snu.ac.kr>
2021-07-05 00:49:37 +0900

    initialize new search page

---
+++ src/screens/SearchStack/Search/Search.tsx
@@ -0,0 +1,98 @@
+import React, { useCallback, useEffect, useState } from 'react';
+
+import { Box, Divider, Input, SearchIcon, VStack } from 'native-base';
+
+import { articleAPI } from '@/apis';
+import { ArticleBox, CursorFlatList } from '@/components';
+import { useCursorPagination } from '@/helpers/hooks';
+import { palette } from '@/styles';
+import { IArticleSummary } from '@/types/article';
+
+import RecentSearch from '../components/RecentSearch';
+import useRecentSearch from '../hooks/useRecentSearch';
+
+const Search: React.FC = () => {
+  const [searchInput, setSearchInput] = useState('');
+  const [searchKeyword, setSearchKeyword] = useState('');
+  const [isSearchResultStage, setSearchResultStage] = useState(false);
+
+  const {
+    items,
+    refreshing,
+    fetching,
+    getItems,
+  } = useCursorPagination<IArticleSummary>({
+    fetchFunc: (url) => articleAPI.getArticles(url, searchKeyword),
+  });
+
+  const {
+    recentSearchKeywords,
+    addRecentSearchKeyword,
+    deleteRecentSearchKeyword,
+  } = useRecentSearch();
+
+  // 초기 렌더링
+  useEffect(() => {
+    getItems('first');
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchKeyword]);
+
+  const handleSearch = useCallback(
+    (keyword?: string) => {
+      setSearchKeyword(keyword ?? searchInput);
+      setSearchResultStage(true);
+      addRecentSearchKeyword(keyword ?? searchInput);
+    },
+    [addRecentSearchKeyword, searchInput]
+  );
+
+  const handleKeywordPress = useCallback(
+    (keyword: string) => {
+      setSearchInput(keyword);
+      handleSearch(keyword);
+    },
+    [handleSearch]
+  );
+
+  const renderArticle = ({ item }: { item: IArticleSummary }) => (
+    <ArticleBox {...item} />
+  );
+
+  return (
+    <VStack backgroundColor={palette.white}>
+      <Box paddingX="20px" paddingY="10px">
+        <Input
+          value={searchInput}
+          placeholder="키워드로 검색"
+          w="100%"
+          variant="filled"
+          InputLeftElement={<SearchIcon m={2} />}
+          onFocus={() => setSearchResultStage(false)}
+          onChangeText={setSearchInput}
+          onSubmitEditing={() => handleSearch(searchInput)}
+        />
+      </Box>
+      <Divider />
+      {isSearchResultStage ? (
+        <CursorFlatList
+          items={items}
+          refreshing={refreshing}
+          isFirstPage={false}
+          fetching={fetching}
+          getItems={getItems}
+          renderItem={renderArticle}
+        />
+      ) : (
+        <VStack>
+          <RecentSearch
+            keywords={recentSearchKeywords}
+            onKeywordDelete={deleteRecentSearchKeyword}
+            onKeywordPress={handleKeywordPress}
+          />
+        </VStack>
+      )}
+    </VStack>
+  );
+};
+
+export default Search;
--- src/screens/SearchStack/SearchedArticle/SearchedList/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function SearchedList(): JSX.Element {
-  return <></>;
-}
-
-export default SearchedList;
