5298f296e3ee7b9c6b39da0758c4b37602599ba5
Hyunsoo Kim <hscornelia@snu.ac.kr>
2021-07-10 09:45:15 +0900

    add image upload hook

---
+++ src/helpers/hooks/useImageUpload.ts
@@ -0,0 +1,81 @@
+import { useState } from 'react';
+
+import { articleAPI } from '@/apis';
+
+const fieldNames = [
+  'key',
+  'x-amz-algorithm',
+  'x-amz-credential',
+  'x-amz-date',
+  'x-amz-security-token',
+  'policy',
+  'x-amz-signature',
+];
+
+/* TODO:
+        remove random key generator when api will change
+*/
+
+interface IImageDict {
+  uri: string;
+  mime: string;
+}
+// type TUseImageUpload = ;
+
+const useImageUpload = (id: number) => {
+  const uploadSingleImage = (image: IImageDict) =>
+    new Promise<string>((resolve) => {
+      articleAPI
+        .putPresignedURL(id, `image_${Math.floor(Math.random() * 1000000)}`)
+        .then(async (res) => {
+          const filename = res.data.file_name;
+          const url = res.data.response.url;
+          const fields = res.data.response.fields;
+
+          // set body fields (for s3 authentication)
+          const body = new FormData();
+          fieldNames.forEach((key) => {
+            body.append(key, fields[key]);
+          });
+
+          // append image file
+          const img = {
+            uri: image.uri,
+            type: image.mime,
+            name: filename as string,
+          };
+          body.append('file', img);
+
+          // send file to s3
+          await fetch(url, {
+            method: 'POST',
+            body: body,
+          })
+            .then((r: any) => {
+              console.log('upsin');
+              resolve(r.headers['map']['location']);
+            })
+            .catch((e) => {
+              console.log('ERROR: inner', e);
+            });
+        })
+        .catch((err) => {
+          console.log('ERROR: ext', err);
+        });
+    });
+
+  const uploadMultipleImages = async (images: IImageDict[]) =>
+    new Promise<string[]>((resolve) => {
+      const promiseArray: Promise<string>[] = images.map((image) => {
+        return uploadSingleImage(image);
+      });
+
+      Promise.all(promiseArray).then((urls) => {
+        resolve(urls);
+      });
+    });
+
+  return { uploadSingleImage, uploadMultipleImages };
+};
+
+export default useImageUpload;
