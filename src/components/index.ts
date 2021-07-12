f8ec37e6bf463926eee635a2fc887f217870e98b
heesu <oung1002@snu.ac.kr>
2021-07-13 00:15:45 +0900

    feat: remove HDRBackBtn

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 export { default as ArticleBox } from './ArticleBox';
 export { default as Button } from './Button';
 export { default as FootTerms } from './FootTerms';
-export { default as HDRBackBtn } from './HDRBackBtn';
 export { default as Header } from './Header';
 export { default as InfoContainer } from './InfoContainer';
 export { default as Profile } from './ProfileBox';
