2297fd4912d5fb9bf351d46f2ad95ceb9fd8b079
heesu <oung1002@snu.ac.kr>
2021-07-14 00:41:10 +0900

    style: update ArticleBox

--- src/components/ArticleBox/index.tsx
+++ src/components/ArticleBox/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { View, TouchableOpacity, Text } from 'react-native';
+import { View, TouchableOpacity } from 'react-native';
 
-import { Image } from 'native-base';
+import { AspectRatio, Flex, Image, Text } from 'native-base';
 
-import { remainTime, calcTimeDiff } from '@/helpers/functions/time';
+import { remainTime } from '@/helpers/functions/time';
 import { useAppNavigation } from '@/helpers/hooks/useAppNavigation';
 import { AppRoutes } from '@/helpers/routes';
-import { $thumnail_size } from '@/styles/size';
 import { IArticleSummary } from '@/types/article';
 
 import styles from './ArticleBox.style';
@@ -17,13 +16,10 @@ const ArticleBox: React.FC<IArticleSummary> = ({
   trading_place,
   time_in,
   images,
+  price_min,
+  article_status,
 }) => {
   const navigation = useAppNavigation();
-
-  const { diff: timeBefore, type: typeBefore } = calcTimeDiff(
-    new Date(),
-    new Date()
-  );
 
   return (
     <TouchableOpacity
@@ -37,25 +33,33 @@ const ArticleBox: React.FC<IArticleSummary> = ({
         })
       }
     >
-      <Image
-        source={{
-          uri: images[0]?.img_url,
-        }}
-        fallbackSource={require('@/assets/images/defaultThumnail.png')}
-        loadingIndicatorSource={require('@/assets/images/defaultThumnail.png')}
-        alt="article thumnail"
-        w={$thumnail_size}
-        h={$thumnail_size}
-      />
+      <AspectRatio ratio={1}>
+        <Image
+          source={{
+            uri: images[0]?.img_url,
+          }}
+          fallbackSource={require('@/assets/images/defaultThumnail.png')}
+          loadingIndicatorSource={require('@/assets/images/defaultThumnail.png')}
+          alt="article thumnail"
+        />
+      </AspectRatio>
       <View style={styles.articleBox}>
-        <Text style={styles.Head}>{title}</Text>
-        <View style={styles.infoWrapper}>
-          <Text style={styles.description}>
-            {`${timeBefore} ${typeBefore} 전`} ·{' '}
+        <Flex>
+          <Flex direction="row">
+            <Text style={styles.Head}>{title}</Text>
+          </Flex>
+          <View style={styles.infoWrapper}>
+            <Text style={styles.description}>{1} 분 전</Text>
+            <Text style={styles.description}>{remainTime(time_in)} · </Text>
+            <Text style={styles.description}>{trading_place}</Text>
+          </View>
+        </Flex>
+        <Flex direction="row" justify="flex-end">
+          <Text style={{ ...styles.priceText }} bold>
+            {article_status.cur_price_sum}원
           </Text>
-          <Text style={styles.description}>{remainTime(time_in)} · </Text>
-          <Text style={styles.description}>{trading_place}</Text>
-        </View>
+          <Text style={styles.priceText}> / {price_min}원</Text>
+        </Flex>
       </View>
     </TouchableOpacity>
   );
