6f06e670345e743d5cf96a988caa050e9a2315e3
Hyunsoo Kim <hscornelia@snu.ac.kr>
2021-07-20 23:22:30 +0900

    handle enter room success and failure

--- src/screens/ArticleStack/Article/Chat.tsx
+++ src/screens/ArticleStack/Article/Chat.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { Text } from 'react-native';
+import { Alert, Text } from 'react-native';
 import { TouchableHighlight } from 'react-native-gesture-handler';
 import { useQuery } from 'react-query';
 
@@ -13,6 +13,7 @@ import GatguWebsocket from '@/helpers/GatguWebsocket/GatguWebsocket';
 import { USER_DETAIL } from '@/queryKeys';
 import { palette } from '@/styles';
 import { IArticleStatus } from '@/types/article';
+import { IChatMessage } from '@/types/chat';
 import { IUserDetail } from '@/types/user';
 
 import styles from './Chat.style';
@@ -24,7 +25,29 @@ interface IChatProps {
 
 function Chat({ article_id, orderStatus }: IChatProps): JSX.Element {
   const navigation = useNavigation();
-  const { sendWsMessage } = GatguWebsocket.useMessage();
+  const { sendWsMessage } = GatguWebsocket.useMessage<{
+    type: string;
+    data: IChatMessage;
+  }>({
+    onmessage: (socket) => {
+      switch (socket.type) {
+        case WSMessage.ENTER_ROOM_SUCCESS: {
+          navigation.navigate('ChattingRoom', {
+            screen: 'ChattingRoom',
+            params: { id: article_id },
+          });
+          break;
+        }
+        case WSMessage.ENTER_ROOM_FAILURE: {
+          Alert.alert("CAN'T ACCESS CHATROOM");
+          break;
+        }
+        default: {
+          console.log('NOPE!', socket);
+        }
+      }
+    },
+  });
   const currentUser = useQuery<IUserDetail>([USER_DETAIL], () =>
     getMyData().then((response) => response.data)
   ).data;
@@ -37,10 +60,6 @@ function Chat({ article_id, orderStatus }: IChatProps): JSX.Element {
           room_id: article_id,
           user_id: currentUser?.id,
         },
-      });
-      navigation.navigate('ChattingRoom', {
-        screen: 'ChattingRoom',
-        params: { id: article_id },
       });
     }
   };
