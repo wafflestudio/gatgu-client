ffcaa0eb3ef6aa84c064b504af81c48f4092b6cf
heesu <oung1002@snu.ac.kr>
2021-08-15 00:18:36 +0900

    feat: fetch userDetail  only when logined

--- src/helpers/hooks/api/userQuery.ts
+++ src/helpers/hooks/api/userQuery.ts
@@ -1,11 +1,17 @@
-import { useQuery } from 'react-query';
+import { useQuery, UseQueryOptions } from 'react-query';
 
 import { getMyData } from '@/apis/UserApi';
 import { USER_DETAIL } from '@/queryKeys';
 import { IUserDetail } from '@/types/user';
 
-export const useUserDetail = () => {
-  return useQuery<IUserDetail>([USER_DETAIL], () =>
-    getMyData().then((response) => response.data)
+import useSelector from '../useSelector';
+
+export const useUserDetail = (options?: UseQueryOptions<IUserDetail>) => {
+  const isLogined = useSelector((state) => state.user.isLogined);
+
+  return useQuery<IUserDetail>(
+    [USER_DETAIL],
+    () => getMyData().then((response) => response.data),
+    { ...options, enabled: isLogined }
   );
 };
