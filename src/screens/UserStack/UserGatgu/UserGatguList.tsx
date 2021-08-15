4b4fc82f3e7870dcf998fd645debe8ce647f3995
heesu <oung1002@snu.ac.kr>
2021-08-15 13:33:42 +0900

    feat: update userGatgu page

--- src/screens/UserStack/UserGatgu/UserGatguList.tsx
+++ src/screens/UserStack/UserGatgu/UserGatguList.tsx
@@ -1,20 +1,29 @@
 import React, { useEffect } from 'react';
 
+import { useNavigation } from '@react-navigation/core';
+
 import { articleAPI } from '@/apis';
 import { ArticleBox, CursorFlatList } from '@/components';
+import { GText } from '@/components/Gatgu';
 import HomeShimmer from '@/components/Shimmer/HomeShimmer';
 import { PAGE_SIZE } from '@/constants/article';
 import { UserArticleActivity } from '@/enums';
 import { useCursorPagination } from '@/helpers/hooks';
+import { AppRoutes } from '@/helpers/routes';
 import { IArticleSummary } from '@/types/article';
+
+import UserGatguEmpty from '../components/UserGatguEmpty/UserGatguEmpty';
 
 interface IUserGatguListProps {
   type: UserArticleActivity;
 }
 
 const UserGatguList: React.FC<IUserGatguListProps> = ({ type }) => {
+  const navigation = useNavigation();
+
   const {
     isFirstPage,
+    isLastPage,
     items,
     firstFetching,
     fetching,
@@ -38,11 +47,47 @@ const UserGatguList: React.FC<IUserGatguListProps> = ({ type }) => {
     return <HomeShimmer />;
   }
 
+  if (!fetching && !items.length) {
+    const title =
+      type === UserArticleActivity.Hosted
+        ? '아직 같구에 참여하지 않으셨네요!'
+        : '같구 글을 올려볼까요?';
+
+    const renderContent = () => {
+      if (type === UserArticleActivity.Hosted) {
+        return (
+          <GText
+            touchable
+            color="gray"
+            textDecorationLine="underline"
+            onPress={() => navigation.navigate(AppRoutes.WriteArticle)}
+          >
+            모집글 쓰러 가기
+          </GText>
+        );
+      }
+
+      return (
+        <GText
+          touchable
+          color="gray"
+          textDecorationLine="underline"
+          onPress={() => navigation.navigate(AppRoutes.Home)}
+        >
+          같구글 확인하기
+        </GText>
+      );
+    };
+
+    return <UserGatguEmpty title={title} content={renderContent()} />;
+  }
+
   return (
     <CursorFlatList
       items={items}
       loading={firstFetching}
       isFirstPage={isFirstPage}
+      isLastPage={isLastPage}
       fetching={fetching}
       getItems={getItems}
       renderItem={renderArticle}
