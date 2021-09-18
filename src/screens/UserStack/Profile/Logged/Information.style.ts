a3c5c0e2c45156579447aefc5baa0f2ac8c188e9
heeedol <oung1002@snu.ac.kr>
2021-09-19 01:49:02 +0900

    style: rchange toaster maxWidth and profile border style

--- src/screens/UserStack/Profile/Logged/Information.style.ts
+++ src/screens/UserStack/Profile/Logged/Information.style.ts
@@ -12,7 +12,7 @@ const infoStyle = StyleSheet.create({
     alignItems: 'center',
     flexDirection: 'row',
     borderColor: palette.borderGray,
-    borderWidth: 1,
+    borderBottomWidth: 1,
     backgroundColor: palette.white,
     overflow: 'hidden',
   },
