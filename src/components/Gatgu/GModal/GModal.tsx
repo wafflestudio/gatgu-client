af196343fa71e70774c160df467188a102592bd3
heeedol <oung1002@snu.ac.kr>
2021-10-03 17:43:37 +0900

    style: update wish price modal style

--- src/components/Gatgu/GModal/GModal.tsx
+++ src/components/Gatgu/GModal/GModal.tsx
@@ -3,30 +3,64 @@ import React from 'react';
 import { Modal } from 'native-base';
 import { IModalProps } from 'native-base/lib/typescript/components/composites/Modal/types';
 
+import { palette } from '@/styles';
+
 type GModalSize = 'small' | 'default' | 'big';
 
 interface GModalContext {
-  size?: GModalSize;
-  type: '';
+  role?: GModalSize;
 }
 
-const GModal: React.FC<IModalProps> = ({ children, ...props }) => {
+type GModalProps = IModalProps & {
+  role?: GModalSize;
+};
+
+const GModalContext = React.createContext<GModalContext>({});
+const useGmodalContext = () => React.useContext(GModalContext);
+
+const GModalProvider: React.FC<GModalContext> = ({
+  role = 'default',
+  children,
+}) => {
   return (
-    <Modal isOpen {...props}>
-      <Modal.Content>{children}</Modal.Content>
-    </Modal>
+    <GModalContext.Provider value={{ role }}>{children}</GModalContext.Provider>
+  );
+};
+
+const GModal: React.FC<GModalProps> = ({ children, role, ...props }) => {
+  return (
+    <GModalProvider>
+      <Modal isOpen size="lg" {...props}>
+        <Modal.Content pb="12px">
+          {typeof props.onClose === 'function' ? (
+            <Modal.CloseButton
+              _pressed={{
+                backgroundColor: palette.whiteGray,
+              }}
+            />
+          ) : null}
+          {children}
+        </Modal.Content>
+      </Modal>
+    </GModalProvider>
   );
 };
 
 const GModalHeader: React.FC = ({ children }) => {
+  const { role } = useGmodalContext();
+
   return <Modal.Header>{children}</Modal.Header>;
 };
 
 const GModalBody: React.FC = ({ children }) => {
-  return <Modal.Body>{children}</Modal.Body>;
+  const { role } = useGmodalContext();
+
+  return <Modal.Body pb="5px">{children}</Modal.Body>;
 };
 
 const GModalFooter: React.FC = ({ children }) => {
+  const { role } = useGmodalContext();
+
   return <Modal.Footer pr={6}>{children}</Modal.Footer>;
 };
 
