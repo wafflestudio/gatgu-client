f83c3bde9bc9e464fe8763fefec9d128359caee0
ldh5545 <eogml5545@naver.com>
2022-06-01 18:17:45 +0900

    add/svg-transformer

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import {Text, View} from 'react-native';
 import {GButton} from '@/components/Gatgu/GButton';
 import LoginStyles from './login.style';
+import Logo from '@/assets/icons/Logo';
 
 const Login = () => {
   return (
     <View style={LoginStyles.login}>
+      <Logo.mainLogo />
       <View style={LoginStyles.kakaoButton}>
-        <GButton theme="yellow">
-          <Text style={{color: 'black', fontSize: 16}}>카카오 로그인</Text>
+        <GButton theme="yellow" textProps={{color: 'dark', size: 16}}>
+          카카오 로그인
         </GButton>
       </View>
     </View>
